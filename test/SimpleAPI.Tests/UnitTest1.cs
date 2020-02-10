bff8c167351ee856e4686c81f68097324bc2254e
santoshmpmd23 <santoshmpmd23@gmail.com>
2020-02-10 15:38:06 +0530

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using SimpleAPI.Controllers;
+using System;
+using Xunit;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("value", returnValue.Value);
+        }
+    }
+}
